3b22c041cb49a43b978e72ea05b941d41c72f4f4
Wesley DeVore <wdevo001@ucr.edu>
2019-01-20 08:39:04 -0800

    Combined Export and master together

--- app/src/main/java/com/example/csync/VisionActivity.java
+++ app/src/main/java/com/example/csync/VisionActivity.java
@@ -2,6 +2,7 @@ package com.example.csync;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class VisionActivity extends AppCompatActivity {
 
@@ -10,4 +11,6 @@ public class VisionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vision_acitvity);
     }
+
+
 }
